aac0a78299873d368b582a8701c784cb0641dc29
David Jin <djin@mitre.org>
2022-08-07 13:25:11 -0400

    added "inbox" column

--- js/config.js
+++ js/config.js
@@ -11,10 +11,10 @@ var config_data = {
     // Sort: Sort order for tasks (default = priority), can state multiple sort keys separated by comma, use '-' to sort descending, Example "duedate,-priority,subject"
     // Restrict: Restrict certain tasks (default = only show incomplete tasks) (More info = https://msdn.microsoft.com/en-us/library/office/ff869597.aspx)
     // Owner: If the task folder is shared by someone else, enter the name of the owner. (i.e. Evren Varol)
-    'BACKLOG_FOLDER':       { Name: '', Title: 'BACKLOG', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: '' },
-    'INPROGRESS_FOLDER': 	{ Name: 'InProgress', Title: 'IN PROGRESS', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: ''},
-    'NEXT_FOLDER': 			{ Name: 'ToDo', Title: 'TO DO', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: ''},
-    'FOCUS_FOLDER': 		{ Name: 'Focus', Title: 'FOCUS', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: '' },
+    'BACKLOG_FOLDER':       { Name: '', Title: 'INBOX', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: '' },
+    'INPROGRESS_FOLDER': 	{ Name: 'ToDo', Title: 'TO DO', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: ''},
+    'NEXT_FOLDER': 			{ Name: 'Backlog', Title: 'BACKLOG', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: ''},
+    'FOCUS_FOLDER': 		{ Name: 'InProgress', Title: 'IN PROGRESS', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: '' },
     'WAITING_FOLDER': 		{ Name: 'Waiting', Title: 'WAITING', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: '' },
     'COMPLETED_FOLDER':     { Name: 'Completed', Title: 'COMPLETED', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: '' },
 
